880ded906d73dedab95248c22bfad534aa4f97eb
WelrodMkII <32021870+WelrodMkII@users.noreply.github.com>
2025-02-03 13:18:40 +0900

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,61 @@
+dofile("expansions/proc_order.lua")
+dofile("expansions/init_Spinel.lua")
+
+dofile("expansions/OriCS-procs/_custom_type.lua")
+dofile("expansions/OriCS-procs/custom_card_type_constants.lua")
+dofile("expansions/OriCS-procs/ygocore_utility.lua")
+
+--[[
+
+UnlimitChain={}
+
+local dschlim=Duel.SetChainLimit
+function Duel.SetChainLimit(f)
+	dschlim(function(e,ep,tp)
+		for _,te in pairs(UnlimitChain) do
+			if e==te then
+				return true
+			end
+		end
+		return f(e,ep,tp)
+	end)
+end
+
+
+local dgcc=Duel.GetCurrentChain
+Auxiliary.CheckDisSumAble=false
+function Duel.GetCurrentChain()
+	if Auxiliary.CheckDisSumAble and dgcc()>0 then
+		return dgcc()-1
+	end
+	return dgcc()
+end
+
+local cregeff=Card.RegisterEffect
+Auxiliary.MetatableEffectCount=true
+function Card.RegisterEffect(c,e,forced,...)
+	local code=c:GetOriginalCode()
+	local mt=_G["c"..code]
+	if c:IsStatus(STATUS_INITIALIZING) and Auxiliary.MetatableEffectCount then
+		if not mt.eff_ct then
+			mt.eff_ct={}
+		end
+		if not mt.eff_ct[c] then
+			mt.eff_ct[c]={}
+		end
+		local ct=0
+		while true do
+			if mt.eff_ct[c][ct]==e then
+				break
+			end
+			if not mt.eff_ct[c][ct] then
+				mt.eff_ct[c][ct]=e
+				break
+			end
+			ct=ct+1
+		end
+	end
+	cregeff(c,e,forced,...)
+end
+
+--]]
