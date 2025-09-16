415394bd410dd67dfbcdcf6a611bc2e44c2ffc8c
WelrodMkII <32021870+WelrodMkII@users.noreply.github.com>
2025-09-16 19:41:55 +0900

    일단 추가

---
+++ script/c99000417.lua
@@ -0,0 +1,6 @@
+--?섎꽆釉뚮씪 ?좏겙
+local s,id=GetID()
+function s.initial_effect(c)
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+end
---
+++ script/c99000427.lua
@@ -0,0 +1,113 @@
+--洹몃┝???띿뿉???쇱뼱?섎뒗 苑껋쿂??
+local s,id=GetID()
+function s.initial_effect(c)
+	--??移대뱶紐낆? 猷곗긽 "?섎꽆釉뚮씪 ?좏겙"?쇰줈??痍④툒?쒕떎.
+	local e0a=Effect.CreateEffect(c)
+	e0a:SetType(EFFECT_TYPE_SINGLE)
+	e0a:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0a:SetCode(EFFECT_ADD_CODE)
+	e0a:SetValue(99000417)
+	c:RegisterEffect(e0a)
+	--?먯떊?대굹 ?곷????깆뿉 移대뱶媛 ?욌㈃?쇰줈 議댁옱??寃쎌슦, ??移대뱶???명듃???댁뿉??諛쒕룞?????덈떎.
+	local e0b=Effect.CreateEffect(c)
+	e0b:SetType(EFFECT_TYPE_SINGLE)
+	e0b:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+	e0b:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e0b:SetCondition(s.actcon)
+	c:RegisterEffect(e0b)
+	--Activate
+	local e1=Fusion.CreateSummonEff({handler=c,
+							fusfilter=aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK),
+							matfilter=Card.IsReleasable,
+							extrafil=s.fextra,
+							extratg=s.extra_target,
+							extraop=s.extra_operation})
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCountLimit(1,id)
+	c:RegisterEffect(e1)
+	--??移대뱶???쇰컲 紐ъ뒪?곌? ?섍퀬, 紐ъ뒪??議댁뿉 ?뱀닔 ?뚰솚?쒕떎.
+	local params={matfilter=Card.IsAbleToRemove,
+				extrafil=s.fextra2,
+				extraop=Fusion.BanishMaterial}
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_RELEASE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop(Fusion.SummonEffTG(params),Fusion.SummonEffOP(params)))
+	c:RegisterEffect(e2)
+end
+s.listed_series={0xc11}
+s.listed_names={99000417}
+function s.actcon(e)
+	local tp=e:GetHandlerPlayer()
+	return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_DECK,LOCATION_DECK,1,nil)
+end
+function s.exfilter(c,lsc,rsc)
+	local lv=c:GetLevel()
+	return lv>lsc and lv<rsc and c:IsReleasable()
+end
+function s.chkfilter(c,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:IsControler(1-tp)
+end
+function s.fcheck(tp,sg,fc)
+	return sg:FilterCount(s.chkfilter,nil,tp)<=1
+end
+function s.fextra(e,tp,mg)
+	if Duel.GetFieldCard(tp,LOCATION_PZONE,0) and Duel.GetFieldCard(tp,LOCATION_PZONE,1) then
+		local lsc=Duel.GetFieldCard(tp,LOCATION_PZONE,0):GetLeftScale()
+		local rsc=Duel.GetFieldCard(tp,LOCATION_PZONE,1):GetRightScale()
+		if lsc>rsc then lsc,rsc=rsc,lsc end
+		local eg=Duel.GetMatchingGroup(s.exfilter,tp,0,LOCATION_MZONE,nil,lsc,rsc)
+		if eg and #eg>0 then
+			return eg,s.fcheck
+		end
+	end
+	return nil
+end
+function s.checkmat(tp,sg,fc)
+	return sg:IsExists(Card.IsSetCard,1,nil,0xc11)
+end
+function s.fextra2(e,tp,mg)
+	if not Duel.IsPlayerAffectedByEffect(tp,CARD_SPIRIT_ELIMINATION) then
+		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil),s.checkmat
+	end
+	return nil
+end
+function s.extra_target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_RELEASE,nil,1,tp,LOCATION_HAND|LOCATION_ONFIELD)
+end
+function s.extra_operation(e,tc,tp,sg)
+	Duel.Release(sg,REASON_EFFECT|REASON_MATERIAL|REASON_FUSION)
+	sg:Clear()
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return not eg:IsContains(e:GetHandler())
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0xc11,TYPE_MONSTER|TYPE_NORMAL,1900,0,3,RACE_BEASTWARRIOR,ATTRIBUTE_LIGHT) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(fusion_target,fusion_operation)
+	return function(e,tp,eg,ep,ev,re,r,rp)
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+		local c=e:GetHandler()
+		if c:IsRelateToEffect(e) and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0xc11,TYPE_MONSTER|TYPE_NORMAL,1900,0,3,RACE_BEASTWARRIOR,ATTRIBUTE_LIGHT) then
+			c:AddMonsterAttribute(TYPE_NORMAL|TYPE_TRAP)
+			Duel.SpecialSummonStep(c,0,tp,tp,true,false,POS_FACEUP)
+			c:AddMonsterAttributeComplete()
+		end
+		Duel.SpecialSummonComplete()
+		if fusion_target(e,tp,eg,ep,ev,re,r,rp,0) and Duel.SelectEffectYesNo(tp,e:GetHandler()) then
+			Duel.BreakEffect()
+			fusion_operation(e,tp,eg,ep,ev,re,r,rp)
+		end
+	end
+end
